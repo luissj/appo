af1d47455a70d91836801d55bf32b78ae39e53f3
luissj <luislipeaz@hotmail.com>
2019-09-16 11:24:40 -0300

    primeira_versao

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Pessoa } from '../pessoa';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  pessoa : Pessoa = new Pessoa();
+
+  constructor() {}
+
+  salvar(){
+    alert('Teste de Salvo');
+  }
+
+}
